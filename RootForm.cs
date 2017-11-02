024786c0809939ef9376f21014a4419bd610ced4
Filip Rak <filiprak@wp.pl>
2017-11-02 12:40:59 +0100

    Starter code: RootForm.

---
+++ RootForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace win_forms
+{
+    public partial class RootForm : Form
+    {
+        public RootForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RootForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
